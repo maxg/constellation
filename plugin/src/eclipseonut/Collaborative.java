5705b5445e9b4998a7d52dff197a29967f0d805b
Max Goldman <maxg@mit.edu>
2014-11-18 11:24:39 -0500

    Ensure that stopping the collaboration will not hang the UI

--- plugin/src/eclipseonut/Collaborative.java
+++ plugin/src/eclipseonut/Collaborative.java
@@ -3,6 +3,8 @@ package eclipseonut;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IFileEditorInput;
@@ -29,9 +31,11 @@ public class Collaborative {
     
     public void stop() {
         try {
-            doc.get().close();
+            doc.get(2, TimeUnit.SECONDS).close();
         } catch (CancellationException ce) {
             // nothing more to stop
+        } catch (TimeoutException te) {
+            Log.error("Leaking pending shared doc during collaboration stop");
         } catch (InterruptedException | ExecutionException e) {
             Log.error("No shared doc during collaboration stop", e);
         }
