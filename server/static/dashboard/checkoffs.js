db68dbf61435c13321d7beeba5d5032054efceb4
Max Goldman <maxg@mit.edu>
2017-01-24 20:31:15 -0500

    Handle checkoffs whose collab is not on the page
    Fixes #43.

--- server/static/dashboard/checkoffs.js
+++ server/static/dashboard/checkoffs.js
@@ -17,10 +17,24 @@ function insertCheckoff(checkoffs, atIndex) {
 
 function updateCheckoff(checkoff) {
   var item = document.getElementById('collab-' + checkoff.data.collabid);
+  if ( ! item) { item = mismatchedCheckoff(checkoff); }
   item.querySelector('.grader').textContent = checkoff.data.grader;
   item.querySelector('.score').innerHTML = '';
   if (checkoff.data.score !== null) {
     var score = document.importNode(document.querySelector('#score-' + checkoff.data.score).content, true);
     item.querySelector('.score').appendChild(score);
   }
+}
+
+function mismatchedCheckoff(checkoff) {
+  var item = document.importNode(document.querySelector('#collab').content, true);
+  var root = item.querySelector('.collab');
+  root.setAttribute('id', 'collab-' + checkoff.data.collabid);
+  var link = item.querySelector('a');
+  var href = '/dashboard/' + project + '/' + checkoff.data.collabid;
+  link.setAttribute('href', href);
+  link.textContent = 'unknown';
+  document.querySelector('#mismatch-header').classList.remove('hidden');
+  document.querySelector('#mismatch').appendChild(item);
+  return root;
 }
