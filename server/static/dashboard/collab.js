720ad63d3a0dbe9bdd2626243fb6333e7b84c4c0
Caitlin Cassidy <ccassidy@mit.edu>
2017-11-17 11:13:35 -0500

    Handle not passing a regex in the URL

--- server/static/dashboard/collab.js
+++ server/static/dashboard/collab.js
@@ -46,116 +46,133 @@ function updateDiff(node, baseline, text, file) {
   if (baseline === undefined || text === undefined) { return; }
   node.innerHTML = '';
 
-  // ';;' is used as the delimiter between regexes
-
-  // TODO: Don't call the regex URL if there are no regexes
-  var allRegexes = regexes ? regexes : '@Override' + ';;' + 'void';
-
-  //allRegexes = '%5C%28.%2A%5C%29'; // \(.*\)
-  $.ajax('/regex/' + collabid + '/' + file.data.filepath + '/' + allRegexes).done(function(regexesJson) {
-    var regexesMap = new Map(JSON.parse(regexesJson));
-
-    // Keep track of the current line number we're on
-    // TODO: -1 doesn't seem right
-    var currentLineNumber = 1;
-
-    // Calculate the diff and highlight it correctly
+  if (!regexes) {
+    // TODO: Make more DRY w/ else code
     window.diff.diffLines(baseline.trim(), text.trim()).forEach(function(part) {
-      var partLines = part.value.split('\n');
-      // Last one is always an empty string
-      partLines.pop();
-
-      //console.log(partLines);
-      //console.log(part);
-      for (var i = 0; i < partLines.length; i++) {
-        var partLine = partLines[i];
-
         var elt = document.createElement('div');
         elt.classList.add('diff-part');
-
-        // A removed part doesn't count toward the line numbers
-        if (part.removed) {
-          elt.classList.add('diff-removed');
-          node.appendChild(elt);
-          //currentLineNumber += 1;
-          continue;
-        }
         if (part.added) {
           elt.classList.add('diff-added');
-        }
-
-        if (regexesMap.has(currentLineNumber)) {
-
-          var endOfLastRegex = 0;
-          regexesMap.get(currentLineNumber).forEach(function(match) {
-            if (endOfLastRegex > match.indexInLine) {
-              // The regexes overlapped (e.g. 'Stream' and 'a')
-              // Ignore this regex
-              // TODO: Better handling of this case?
-              //   Probably won't happen that much?
-
-              // TODO: What if the regex that's first in the line
-              //   came last
-              
-              return;
-            }
-
-            // TODO: Make more DRY
-
-            var beforeRegexElt = document.createElement('span');
-            var regexElt = document.createElement('span');
-            var afterRegexElt = document.createElement('span');
-
-            beforeRegexElt.appendChild(document.createTextNode(
-              partLine.substring(endOfLastRegex, match.indexInLine)));
-            regexElt.appendChild(document.createTextNode(
-              partLine.substring(match.indexInLine, match.indexInLine + match.length)));
-            afterRegexElt.appendChild(document.createTextNode(
-              partLine.substring(match.indexInLine + match.length)));
-
-            regexElt.classList.add('diff-regex');
-
-            if (endOfLastRegex > 0) {
-              // Need to remove the last child, since this the three elts
-              // created here represent the same characters as the last child
-              elt.removeChild(elt.lastChild);
-            }
-
-            elt.appendChild(beforeRegexElt);
-            elt.appendChild(regexElt);
-            elt.appendChild(afterRegexElt);
-
-            endOfLastRegex = match.indexInLine + match.length;
-          });
-
-          // Add newline back in for correct syntax highlighting
-          elt.lastChild.appendChild(document.createTextNode('\n'));
-
+          elt.appendChild(document.createTextNode(part.value));
+        } else if (part.removed) {
+          elt.classList.add('diff-removed');
         } else {
+          elt.appendChild(document.createTextNode(part.value));
+        }
+        node.appendChild(elt);
+      });
+      hljs.highlightBlock(node);
+
+  } else {
+    // ';;' is used as the delimiter between regexes
+    //regexes = '%5C%28.%2A%5C%29'; // \(.*\)
+    $.ajax('/regex/' + collabid + '/' + file.data.filepath + '/' + regexes).done(function(regexesJson) {
+      var regexesMap = new Map(JSON.parse(regexesJson));
+
+      // Keep track of the current line number we're on
+      // TODO: -1 doesn't seem right
+      var currentLineNumber = 1;
+
+      // Calculate the diff and highlight it correctly
+      window.diff.diffLines(baseline.trim(), text.trim()).forEach(function(part) {
+        var partLines = part.value.split('\n');
+        // Last one is always an empty string
+        partLines.pop();
+
+        //console.log(partLines);
+        //console.log(part);
+        for (var i = 0; i < partLines.length; i++) {
+          var partLine = partLines[i];
+
+          var elt = document.createElement('div');
           elt.classList.add('diff-part');
+
+          // A removed part doesn't count toward the line numbers
+          if (part.removed) {
+            elt.classList.add('diff-removed');
+            node.appendChild(elt);
+            //currentLineNumber += 1;
+            continue;
+          }
           if (part.added) {
             elt.classList.add('diff-added');
           }
 
-          // Add newline back in for correct syntax highlighting
-          elt.appendChild(document.createTextNode(partLine + '\n'));
+          if (regexesMap.has(currentLineNumber)) {
+
+            var endOfLastRegex = 0;
+            regexesMap.get(currentLineNumber).forEach(function(match) {
+              if (endOfLastRegex > match.indexInLine) {
+                // The regexes overlapped (e.g. 'Stream' and 'a')
+                // Ignore this regex
+                // TODO: Better handling of this case?
+                //   Probably won't happen that much?
+
+                // TODO: What if the regex that's first in the line
+                //   came last
+                
+                return;
+              }
+
+              // TODO: Make more DRY
+
+              var beforeRegexElt = document.createElement('span');
+              var regexElt = document.createElement('span');
+              var afterRegexElt = document.createElement('span');
+
+              beforeRegexElt.appendChild(document.createTextNode(
+                partLine.substring(endOfLastRegex, match.indexInLine)));
+              regexElt.appendChild(document.createTextNode(
+                partLine.substring(match.indexInLine, match.indexInLine + match.length)));
+              afterRegexElt.appendChild(document.createTextNode(
+                partLine.substring(match.indexInLine + match.length)));
+
+              regexElt.classList.add('diff-regex');
+
+              if (endOfLastRegex > 0) {
+                // Need to remove the last child, since this the three elts
+                // created here represent the same characters as the last child
+                elt.removeChild(elt.lastChild);
+              }
+
+              elt.appendChild(beforeRegexElt);
+              elt.appendChild(regexElt);
+              elt.appendChild(afterRegexElt);
+
+              endOfLastRegex = match.indexInLine + match.length;
+            });
+
+            // Add newline back in for correct syntax highlighting
+            elt.lastChild.appendChild(document.createTextNode('\n'));
+
+          } else {
+            elt.classList.add('diff-part');
+            if (part.added) {
+              elt.classList.add('diff-added');
+            }
+
+            // Add newline back in for correct syntax highlighting
+            elt.appendChild(document.createTextNode(partLine + '\n'));
+          }
+
+          node.appendChild(elt);
+          currentLineNumber += 1;
         }
+        
+      });
 
-        node.appendChild(elt);
-        currentLineNumber += 1;
-      }
+      // TODO: Syntax highlighting doesn't work anymore
+      hljs.highlightBlock(node);
+
       
+      
+    }).fail(function(req, status, err) {
+      console.log("got regex error");
+      console.log(err);
     });
+  }
 
-    // TODO: Syntax highlighting doesn't work anymore
-    hljs.highlightBlock(node);
-
-    
-    
-  }).fail(function(req, status, err) {
-    console.log("got regex error");
-    console.log(err);
-  });
+  
 
   
 
