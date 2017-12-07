25c7e230689fe805b73b4c03ffa06d83f95d7ff0
Caitlin Cassidy <ccassidy@mit.edu>
2017-12-07 11:59:46 -0500

    Add back code to show baseline Constellation visual

--- server/static/dashboard/collab.js
+++ server/static/dashboard/collab.js
@@ -10,8 +10,6 @@ collab.fetch(function(err) {
 connection.createFetchQuery('files', { collabid: collabid }, {}, function(err, files) {
   if (err) { throw err; }
   
-  // TODO: Do the file.subscribe stuff to update the view
-
   // Example for using the visual parameter to show different visualizations
   if (visual == 1) {
     showFiles_visual1(files);
@@ -23,6 +21,41 @@ connection.createFetchQuery('files', { collabid: collabid }, {}, function(err, f
 });
 
 function showFiles_basic(files) {
+  var list = document.querySelector('#files');
+  files.sort(function(a, b) { return a.data.filepath.localeCompare(b.data.filepath); });
+  files.forEach(function(file) {
+    var item = document.importNode(document.querySelector('#file').content, true);
+    var heading = item.querySelector('h4');
+    heading.textContent = file.data.filepath;
+    var diff = item.querySelector('.diff code');
+    list.appendChild(item);
+    
+    $.ajax('/baseline/' + project + '/' + file.data.filepath).done(function(baseline) {
+      if (cutoff) {
+        $.ajax('/historical/' + project + '/' + collabid + '/' + file.data.filepath + '/' + cutoff).done(function(historical) {
+          updateDiff(diff, baseline, historical.data ? historical.data.text : undefined);
+        }).fail(function(req, status, err) {
+          diff.textContent = 'Error fetching code: ' + errorToString(req.responseJSON, status, err);
+        });
+      } else {
+        file.subscribe(function() {
+          updateDiff(diff, baseline, file.data.text);
+          file.on('op', function() {
+            updateDiff(diff, baseline, file.data.text);
+          });
+        });
+      }
+    }).fail(function(req, status, err) {
+      diff.textContent = 'Error fetching baseline: ' + errorToString(req.responseJSON, status, err);
+    });
+  });
+}
+
+
+/** Visual 1: Total diff */
+function showFiles_visual1(files) {
+  // TODO: Also do the subscribe thing
+
   var list = document.querySelector('#files');
   files.sort(function(a, b) { return a.data.filepath.localeCompare(b.data.filepath); });
   files.forEach(function(file) {
@@ -53,22 +86,12 @@ function showFiles_basic(files) {
         codeBlock.appendChild(elt);
       });
 
+      // TODO: Add syntax highlighting?
+
     }).fail(function(req, status, err) {
       list.textContent = 'Error fetching ops: ' + errorToString(req.responseJSON, status, err);
     });
 
-  });
-}
-
-function showFiles_visual1(files) {
-  var list = document.querySelector('#files');
-
-  files.sort(function(a, b) { return a.data.filepath.localeCompare(b.data.filepath); });
-  files.forEach(function(file) {
-    var item = document.importNode(document.querySelector('#file').content, true);
-    var heading = item.querySelector('h4');
-    heading.textContent = file.data.filepath + " VISUALZATION 1";
-    list.appendChild(item);
   });
 
 }
@@ -84,6 +107,26 @@ function showFiles_visual2(files) {
     list.appendChild(item);
   });
 
+}
+
+// Used for basic visual
+function updateDiff(node, baseline, text) {
+  if (baseline === undefined || text === undefined) { return; }
+  node.innerHTML = '';
+  window.diff.diffLines(baseline.trim(), text.trim()).forEach(function(part) {
+    var elt = document.createElement('div');
+    elt.classList.add('diff-part');
+    if (part.added) {
+      elt.classList.add('diff-added');
+      elt.appendChild(document.createTextNode(part.value));
+    } else if (part.removed) {
+      elt.classList.add('diff-removed');
+    } else {
+      elt.appendChild(document.createTextNode(part.value));
+    }
+    node.appendChild(elt);
+  });
+  hljs.highlightBlock(node);
 }
 
 
