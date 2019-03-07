8848dd186a368798392c0569c86981cc45a2e697
Max Goldman <maxg@mit.edu>
2019-03-07 11:13:34 -0500

    Update instance type to increase RAM

--- setup/terraform.tf
+++ setup/terraform.tf
@@ -108,7 +108,7 @@ resource "aws_key_pair" "app" {
 }
 
 resource "aws_instance" "web" {
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   ami = "${data.aws_ami.web.id}"
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
   subnet_id = "${aws_subnet.a.id}"
